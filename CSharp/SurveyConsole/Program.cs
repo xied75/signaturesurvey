53b3da90440188af3715d4c6c5557f492171d50b
cessor <cessor@localhost>
2012-09-23 16:31:14 +0200

    Got the survey to work. The implementation looks too imperative

--- CSharp/SurveyConsole/Program.cs
+++ CSharp/SurveyConsole/Program.cs
@@ -9,8 +9,12 @@ namespace SurveyConsole
     {
         private static void Main(string[] args)
         {
-            var path = args[1];
+            var path = args[0];
             var chars = new List<char> {'{', '}', ';'}.ToList();
+
+            var loc = File.ReadAllLines(path).Length;
+            Console.Write("{0} ({1}): ", Path.GetFileName(path), loc);
+
             using (var file = File.OpenRead(path))
             {
                 for (var i = 0; i < file.Length; i++)
@@ -20,7 +24,6 @@ namespace SurveyConsole
                         Console.Write(c);
                 }
             }
-            Console.ReadKey();
         }
     }
 }
